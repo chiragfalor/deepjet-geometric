e488c5c3ed44a333527957df626b0c1e2a9b255a
Irina Shenogina <ishenogi@submit20.mit.edu>
2022-04-22 06:44:10 -0400

    included upuppi init file

--- deepjet_geometric/datasets/__init__.py
+++ deepjet_geometric/datasets/__init__.py
@@ -1,7 +1,7 @@
 from .djc_v1_dataset import DeepJetCoreV1
 from .djc_v2_dataset import DeepJetCoreV2
 from .met_v1_dataset import MetV1
-from .ticl_v1_dataset import TICLV1
 from .suep_v1_dataset import SUEPV1
+from .upuppi_v0_dataset import UPuppiV0
 
-__all__ = ['DeepJetCoreV1', 'DeepJetCoreV2', 'MetV1', 'TICLV1', 'SUEPV1']
+__all__ = ['DeepJetCoreV1', 'DeepJetCoreV2', 'MetV1', 'TICLV1', 'SUEPV1', 'UPuppiV0']
